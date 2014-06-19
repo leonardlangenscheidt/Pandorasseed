c26f25d6f6b988cbecd06ee7749cea0ec45fe8ec
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2014-06-19 17:15:40 -0400

    env keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_ryUYGDCxguN4NGM4BVXPf61T',
-  :secret_key      => 'sk_test_iHEjq1dUXDrqpzC8yIattMIm'
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
